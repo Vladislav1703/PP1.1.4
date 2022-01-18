81ec4cb883a3ef4915c8a86d3ba97b99961e9de5
Bulavintsev <v.bulavintsev@kt-team.de>
2022-01-18 12:22:36 +0400

    небольшие правки код стайла

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -1,11 +1,11 @@
 package jm.task.core.jdbc.dao;
 
-        import jm.task.core.jdbc.model.User;
-        import jm.task.core.jdbc.util.Util;
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
 
-        import java.sql.*;
-        import java.util.ArrayList;
-        import java.util.List;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
     public UserDaoJDBCImpl() {
