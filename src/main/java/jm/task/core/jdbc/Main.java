33cb25b9841150e5bbc93020d3e3715256091931
Bulavintsev <v.bulavintsev@kt-team.de>
2022-01-18 12:28:44 +0400

    завязался на работу сервиса

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,19 +1,19 @@
 package jm.task.core.jdbc;
 
-import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+import jm.task.core.jdbc.service.UserServiceImpl;
 
 public class Main {
     public static void main(String[] args) {
-        UserDaoJDBCImpl userDao = new UserDaoJDBCImpl();
+        UserServiceImpl userService = new UserServiceImpl();
         try {
-            userDao.createUsersTable();
-            userDao.saveUser("Egor", "Letov", (byte) 43);
-            userDao.saveUser("Xi", "Jinping", (byte) 68);
-            userDao.saveUser("Dimem", "Mem", (byte) 25);
-            userDao.saveUser("Petr", "Ivanov", (byte) 88);
-            System.out.println(userDao.getAllUsers());
-            userDao.cleanUsersTable();
-            userDao.dropUsersTable();
+            userService.createUsersTable();
+            userService.saveUser("Egor", "Letov", (byte) 43);
+            userService.saveUser("Xi", "Jinping", (byte) 68);
+            userService.saveUser("Dimem", "Mem", (byte) 25);
+            userService.saveUser("Petr", "Ivanov", (byte) 88);
+            System.out.println(userService.getAllUsers());
+            userService.cleanUsersTable();
+            userService.dropUsersTable();
         } catch (Exception e) {
             System.out.println("bad :(");
         }
